0a8a26456ac90c811640f83f5883043fa28c60b3
mayangayustia <mayangayustia@gmail.com>
2024-09-29 22:16:08 +0700

    Update latihan1.2.php

--- latihan1.2.php
+++ latihan1.2.php
@@ -3,14 +3,14 @@ Class Mobil{
     //ini adalah atribut/properties dari mobil
     var $jumlahRoda=4;
     var $warna="Merah";
-    var $bahanBakar="Pertamax"
+    var $bahanBakar="Pertamax";
     var $harga=120000000;
     var $merek='A';
 
     //deklarasi method
     public function statusHarga()
     {
-        if(this->harga > 50000000) $status = 'Mahal';
+        if($this->harga > 50000000) $status = 'Mahal';
         else $status = 'Murah';
         return $status;
     }
