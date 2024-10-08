e03cc7c5f1ff7ea8ab84ad58e049d07518774737
mayangayustia <mayangayustia@gmail.com>
2024-10-08 23:23:30 +0700

    Update latihan2.17.php

--- latihan2.17.php
+++ latihan2.17.php
@@ -1,6 +1,6 @@
 <?php
- $x = array("a" => "red", "b" => "green");
-$x = array("c" => "blue", "d" => "yellow");
+$x = array("a" => "red", "b" => "green");
+$y = array("c" => "blue", "d" => "yellow");
 var_dump($x != $y);
 echo "<br>";
 var_dump($x === $y);
